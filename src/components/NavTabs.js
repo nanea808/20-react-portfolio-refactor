4b0629eafe1fefc57dd97040ef56ad38b0d5f908
nanea808 <willers.enw@gmail.com>
2023-03-24 03:19:16 -0700

    css imports properly, refactored nav bar

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 // const styles = {
 //   nav: {
@@ -12,50 +12,55 @@ import React from 'react';
 //     padding: '1rem, 0.5rem'
 //   },
 //   navLink: {
-    
+
 //   }
 // }
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul>
-      <li>
-        <a
-          href="#home"
-          onClick={() => handlePageChange('Home')}
-          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-        >
-          Home
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => handlePageChange('About')}
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-        >
-          About
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#blog"
-          onClick={() => handlePageChange('Blog')}
-          className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
-        >
-          Blog
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#contact"
-          onClick={() => handlePageChange('Contact')}
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Contact
-        </a>
-      </li>
-    </ul>
+    <div className="nav">
+      <h1>Ethan Willers</h1>
+      <ul>
+        <li>
+          <a
+            href="#home"
+            onClick={() => handlePageChange("Home")}
+            className={currentPage === "Home" ? "nav-link active" : "nav-link"}
+          >
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#about"
+            onClick={() => handlePageChange("About")}
+            className={currentPage === "About" ? "nav-link active" : "nav-link"}
+          >
+            About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#blog"
+            onClick={() => handlePageChange("Blog")}
+            className={currentPage === "Blog" ? "nav-link active" : "nav-link"}
+          >
+            Blog
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#contact"
+            onClick={() => handlePageChange("Contact")}
+            className={
+              currentPage === "Contact" ? "nav-link active" : "nav-link"
+            }
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './components/index.css';
+import './components/reset.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
